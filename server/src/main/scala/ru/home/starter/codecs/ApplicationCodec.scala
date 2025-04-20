43c8fa3a0ed6c99e719b350aa630f49e459b8bc2
Igor Kioresko <ikioresko@gmail.com>
2025-04-20 16:25:39 +0500

    refactor: app codec

--- server/src/main/scala/ru/home/starter/codecs/ApplicationCodec.scala
+++ server/src/main/scala/ru/home/starter/codecs/ApplicationCodec.scala
@@ -1,13 +1,16 @@
 package ru.home.starter.codecs
 
-import io.circe.generic.semiauto.deriveEncoder
-import io.circe.{Codec, Decoder}
+import io.circe.syntax.EncoderOps
+import io.circe.{Codec, Decoder, Encoder, Json}
 import sttp.tapir.json.circe.TapirJsonCirce
 
-trait ApplicationCodec extends TapirJsonCirce with ResponseCodec {
+trait ApplicationCodec extends TapirJsonCirce with ResponseCodec with TapirSchema {
 
-  implicit val jsonThrowableCodec: Codec[Throwable] = Codec.from(
-    Decoder.failedWithMessage("Not implemented"),
-    deriveEncoder
-  )
+  implicit val jsonThrowableCodec: Codec[Throwable] = {
+    Codec.from(
+      Decoder { cursor => cursor.downField("Message").as[String].map(new Throwable(_)) },
+      Encoder { _ => Json.obj("Message" -> "Something went wrong".asJson) }
+    )
+  }
+
 }
