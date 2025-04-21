f22ee2cb73e26b7faf8cdb73f10a80bb94dde193
Igor Kioresko <ikioresko@gmail.com>
2025-04-21 10:29:47 +0500

    feat: about endpoint test

---
+++ server/src/test/scala/ru/home/starter/endpoints/AboutEndpointSpec.scala
@@ -0,0 +1,38 @@
+package ru.home.starter.endpoints
+
+import cats.data.EitherT
+import cats.effect.IO
+import io.circe.syntax.EncoderOps
+import ru.home.starter.endpoints.api.AboutEndpoint
+import ru.home.starter.haldlers.AboutHandler
+import ru.home.starter.models.AboutResponse
+import sttp.client3._
+import sttp.model.StatusCode
+
+class AboutEndpointSpec extends EndpointSpec {
+
+  private val (handler, backend) = {
+    val handler = mock[AboutHandler[IO]]
+    val endpoints = new AboutEndpoint[IO](handler).endpoints
+    val backend = sttpBackend(endpoints)
+
+    (handler, backend)
+  }
+
+  private val expected = AboutResponse("1.0-SNAPSHOT")
+
+  "getAboutInfo" should "return info" in {
+    handler.getAboutInfo
+      .returns(EitherT.rightT(expected))
+
+    quickRequest
+      .get(uri"$baseUrl/about")
+      .send(backend)
+      .asserting { response =>
+        response.body shouldBe expected.asJson.noSpaces
+        response.code shouldBe StatusCode.Ok
+      }
+      .assertNoException
+  }
+
+}
--- server/src/test/scala/ru/home/starter/endpoints/EndpointSpec.scala
+++ server/src/test/scala/ru/home/starter/endpoints/EndpointSpec.scala
@@ -3,9 +3,10 @@ package ru.home.starter.endpoints
 import cats.effect.IO
 import cats.effect.testing.scalatest.AsyncIOSpec
 import org.mockito.cats.IdiomaticMockitoCats
-import org.mockito.scalatest.AsyncIdiomaticMockito
+import org.mockito.scalatest.{AsyncIdiomaticMockito, ResetMocksAfterEachAsyncTest}
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
+import ru.home.starter.codecs.ApplicationCodec
 import sttp.client3.SttpBackend
 import sttp.client3.httpclient.fs2.HttpClientFs2Backend
 import sttp.client3.impl.cats.CatsImplicits
@@ -18,11 +19,13 @@ trait EndpointSpec
     with AsyncIOSpec
     with Matchers
     with AsyncIdiomaticMockito
-    with IdiomaticMockitoCats {
+    with IdiomaticMockitoCats
+    with ResetMocksAfterEachAsyncTest
+    with ApplicationCodec {
 
   protected val baseUrl = "http://localhost:9090/v1"
 
-  protected def makeBackend(endpoints: List[ServerEndpoint[Any, IO]]): SttpBackend[IO, Any] = {
+  protected def sttpBackend(endpoints: List[ServerEndpoint[Any, IO]]): SttpBackend[IO, Any] = {
     TapirStubInterpreter(HttpClientFs2Backend.stub[IO])
       .whenServerEndpointsRunLogic(endpoints)
       .backend()
