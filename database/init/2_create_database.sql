a71f3c83607bcd348f2f911c87eb28c33f452f76
Igor Kioresko <ikioresko@gmail.com>
2025-04-25 10:58:24 +0500

    feat: add database initialization

---
+++ database/init/2_create_database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE starter OWNER starter;
---
+++ database/init/3_schema_initialization.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS app_users (
+  "id" SERIAL PRIMARY KEY,
+  "login" VARCHAR(32) UNIQUE NOT NULL,
+  "email" VARCHAR(128)
+);
