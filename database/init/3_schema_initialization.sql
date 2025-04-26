ed092430a856c9240b3137d0e5e418691d655a39
Igor Kioresko <ikioresko@gmail.com>
2025-04-26 09:54:30 +0500

    refactor: schema initialization

--- database/init/3_schema_initialization.sql
+++ database/init/3_schema_initialization.sql
@@ -1,5 +1,9 @@
+\c starter starter
+
 CREATE TABLE IF NOT EXISTS app_users (
   "id" SERIAL PRIMARY KEY,
   "login" VARCHAR(32) UNIQUE NOT NULL,
   "email" VARCHAR(128)
 );
+
+INSERT INTO app_users VALUES (0, 'admin');
