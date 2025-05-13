5a2c53e3a996f70d17d08e839d6ce669b1dd7c48
Igor Kioresko <ikioresko@gmail.com>
2025-05-13 09:26:13 +0500

    feat: add StarterWorker

---
+++ core/src/main/scala/ru/home/starter/worker/StarterWorker.scala
@@ -0,0 +1,33 @@
+package ru.home.starter.worker
+
+import cats.effect.Async
+import cats.syntax.all._
+import cats.effect.kernel.Resource
+import org.typelevel.log4cats.LoggerFactory
+
+import java.time.LocalDateTime
+import scala.concurrent.duration.FiniteDuration
+
+class StarterWorker[F[_]: LoggerFactory](interval: FiniteDuration)(implicit private val F: Async[F]) {
+  private val L = LoggerFactory.getLogger
+
+  def start(): Resource[F, Unit] = {
+    def workWithInterval: F[Unit] = {
+      F.sleep(interval) >> L.info(s"${LocalDateTime.now()}")
+    }
+
+    F.background {
+      L.info(s"Worker has started with interval: '$interval'") >>
+        workWithInterval.foreverM.void
+    }.void
+  }
+
+}
+
+object StarterWorker {
+
+  def apply[F[_]: Async: LoggerFactory](interval: FiniteDuration): Resource[F, StarterWorker[F]] = {
+    Resource.pure(new StarterWorker(interval))
+  }
+
+}
