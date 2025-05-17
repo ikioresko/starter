e2d7f8b7f46d3f49c4b0a6e2a36bf95311e2e978
Igor Kioresko <ikioresko@gmail.com>
2025-05-17 12:23:57 +0500

    refactor: AboutServiceSpec

--- core/src/test/scala/ru/home/starter/services/app/AboutServiceSpec.scala
+++ core/src/test/scala/ru/home/starter/services/app/AboutServiceSpec.scala
@@ -1,18 +1,25 @@
 package ru.home.starter.services.app
 
 import cats.effect.IO
+import fs2.io.file.Path
 import ru.home.starter.BaseSpec
 import ru.home.starter.services.entity.VersionInfo
 import ru.home.starter.services.file.FileManager
 
 class AboutServiceSpec extends BaseSpec {
   private val fileManager = mock[FileManager[IO]]
+  private val version = "1.0"
 
-  "getAboutInfo" should "return info" in {
-    val version = "1.0"
-
+  "getVersionInfo" should "return version" in {
     new AboutService[IO](version, fileManager).getVersionInfo
       .map(_ shouldBe VersionInfo(version))
+  }
+
+  "getConfigInfo" should "return config" in {
+    fileManager.readFile(any[Path]).returnsF("")
+
+    new AboutService[IO](version, fileManager).getConfigInfo
+      .map(_ shouldBe "")
   }
 
 }
