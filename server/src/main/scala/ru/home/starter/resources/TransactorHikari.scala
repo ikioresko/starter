ee3e626450cb8b58e5976aaa8f22211158f9afb9
Igor Kioresko <ikioresko@gmail.com>
2025-05-25 14:55:53 +0500

    feat: add doobie logHandler

--- server/src/main/scala/ru/home/starter/resources/TransactorHikari.scala
+++ server/src/main/scala/ru/home/starter/resources/TransactorHikari.scala
@@ -1,14 +1,17 @@
 package ru.home.starter.resources
 
 import cats.effect.{Async, Resource}
+import cats.implicits._
 import doobie.Transactor
 import doobie.hikari.HikariTransactor
-import doobie.util.ExecutionContexts
+import doobie.util.log.LogHandler
+import doobie.util.{ExecutionContexts, log}
+import org.typelevel.log4cats.LoggerFactory
 import ru.home.starter.configs.AppConfig
 
 object TransactorHikari {
 
-  def apply[F[_]: Async](appConfig: AppConfig): Resource[F, Transactor[F]] = {
+  def apply[F[_]: Async: LoggerFactory](appConfig: AppConfig): Resource[F, Transactor[F]] = {
     for {
       ec <- ExecutionContexts.fixedThreadPool[F](appConfig.transactor.threadPoolSize)
       hikari <- HikariTransactor.newHikariTransactor[F](
@@ -16,9 +19,42 @@ object TransactorHikari {
         appConfig.database.url,
         appConfig.database.user,
         appConfig.database.password,
-        ec
+        ec,
+        logHandler.some
       )
     } yield hikari
+  }
+
+  private def logHandler[F[_]: LoggerFactory]: LogHandler[F] = new LogHandler[F] {
+    private val L = LoggerFactory.getLogger
+
+    override def run(logEvent: log.LogEvent): F[Unit] = {
+      logEvent match {
+        case log.Success(sql, params, _, _, _) =>
+          L.debug(s"""Success:
+                     | ${sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
+                     |
+                     | params = [${params.allParams.flatten.mkString(", ")}]
+              """.stripMargin)
+        case log.ProcessingFailure(sql, params, label, _, _, failure) =>
+          L.error(s"""ProcessingFailure:
+                     |  ${sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
+                     |
+                     | params = [${params.allParams.flatten.mkString(", ")}]
+                     | label = $label
+                     | failure = ${failure.getMessage}
+              """.stripMargin)
+        case log.ExecFailure(sql, params, label, _, failure) =>
+          L.error(s"""ExecFailure:
+                     |  ${sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
+                     |
+                     | params = [${params.allParams.flatten.mkString(", ")}]
+                     | label = $label
+                     | failure = ${failure.getMessage}
+              """.stripMargin)
+      }
+    }
+
   }
 
 }
