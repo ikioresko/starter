985beb5746849d2149aef6c7d62031128e531b4e
Igor Kioresko <ikioresko@gmail.com>
2025-05-28 10:29:59 +0500

    feat: RedisClient

---
+++ core/src/main/scala/ru/home/starter/clients/RedisClient.scala
@@ -0,0 +1,40 @@
+package ru.home.starter.clients
+
+import cats.effect.{Async, Resource}
+import dev.profunktor.redis4cats.Redis
+import dev.profunktor.redis4cats.algebra.StringCommands
+import dev.profunktor.redis4cats.connection.{RedisClient => Redis4C}
+import dev.profunktor.redis4cats.data.RedisCodec
+import dev.profunktor.redis4cats.effect.Log.Stdout._
+import io.lettuce.core.{ClientOptions, TimeoutOptions}
+
+import java.time.Duration
+
+object RedisClient {
+
+  def apply[F[_]: Async](): Resource[F, StringCommands[F, String, String]] = {
+    val stringCodec: RedisCodec[String, String] = RedisCodec.Utf8
+
+    val mkOpts: F[ClientOptions] =
+      Async[F].delay {
+        ClientOptions
+          .builder()
+          .autoReconnect(false)
+          .pingBeforeActivateConnection(false)
+          .timeoutOptions(
+            TimeoutOptions
+              .builder()
+              .fixedTimeout(Duration.ofSeconds(10))
+              .build()
+          )
+          .build()
+      }
+
+    for {
+      opts <- Resource.eval(mkOpts)
+      client <- Redis4C[F].withOptions("redis://localhost", opts)
+      redis <- Redis[F].fromClient(client, stringCodec)
+    } yield redis
+  }
+
+}
